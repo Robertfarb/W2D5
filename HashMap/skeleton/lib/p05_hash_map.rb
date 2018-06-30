fa2e612c8be6de41f2104b78def1a976aa8dad4b
robertfarb <farb.robert@gmail.com>
2018-06-29 17:03:42 -0700

    Begin creating hash_map

--- HashMap/skeleton/lib/p05_hash_map.rb
+++ HashMap/skeleton/lib/p05_hash_map.rb
@@ -1,4 +1,6 @@
 require_relative 'p04_linked_list'
+require_relative 'p02_hashing'
+require 'byebug'
 
 class HashMap
   attr_reader :count
@@ -9,27 +11,41 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    bucket = bucket(key)
+    if bucket.include?(key)
+      bucket.update(key, val)
+    else
+      bucket.append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).each {|el| return el.val if el.key == key}
+    nil
   end
 
   def delete(key)
+    if bucket(key).include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
   def each
   end
 
   # uncomment when you have Enumerable included
-  def to_s
-    pairs = inject([]) do |strs, (k, v)|
-      strs << "#{k.to_s} => #{v.to_s}"
-    end
-    "{\n" + pairs.join(",\n") + "\n}"
-  end
+  # def to_s
+  #   pairs = inject([]) do |strs, (k, v)|
+  #     strs << "#{k.to_s} => #{v.to_s}"
+  #   end
+  #   "{\n" + pairs.join(",\n") + "\n}"
+  # end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -45,5 +61,6 @@ class HashMap
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
